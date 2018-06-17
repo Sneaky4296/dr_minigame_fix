e31078e8483e0abaf81cf02d7b875de1a9da5029
Jonah <jonahverner@gmail.com>
2018-06-16 23:49:35 -0500

    Set DR round limit and fix bugs
    Fixed mistype, now sets grav to 600 when not DR and 800 when DR. Also sets round limit to 20 which should keep the built in map voting from working.

--- lua/autorun/server/drfix.lua
+++ lua/autorun/server/drfix.lua
@@ -1,7 +1,11 @@
 local function setGrav()
   local mode = GAMEMODE
-  if mode == "deathrun" then
+  if mode ~= "Deathrun" then
     RunConsoleCommand("sv_gravity", "600")
+  end
+  if mode =="Deathrun" then
+    RunConsoleCommand("deathrun_round_limit", "20")
+    RunConsoleCommand("sv_gravity", "800")
   end
 end
 hook.Add("PostGamemodeLoaded", "setGrav", setGrav)
