6659be7993301422947975cb7db9d1e49f02043b
Jonah <jonahverner@gmail.com>
2018-06-27 10:10:34 -0500

    Removed vote and round limit functions
    Now a part of my kmapvote_compatibility project

--- lua/autorun/server/drfix.lua
+++ lua/autorun/server/drfix.lua
@@ -5,16 +5,8 @@ local function setConvars()
     RunConsoleCommand("sv_airaccelerate", "10")
   end
   if mode =="Deathrun" then
-    RunConsoleCommand("deathrun_round_limit", "5")
     RunConsoleCommand("sv_gravity", "800")
   end
 end
 
-local function gameVote( rounds )
-  if rounds >= 5 then
-    MAPVOTE.setState("VoteGamemode")
-    return true
-  end
-end
 hook.Add("Initialize", "setConvars", setConvars)
-hook.Add("DeathrunStartMapvote", "triggerKMapVote", gameVote)
